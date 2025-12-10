eeb3d971c56d2723b34c9d4046ccd56c07ba19a1
Vanessa Zampieri <vanessazampieri01@gmail.com>
2025-12-10 01:28:24 +0000

    Exercício 07 - media

---
+++ Exercicio07.py
@@ -0,0 +1,6 @@
+a = int(input("Digite o primeiro numero: "))
+b = int(input("Digite o segundo numero: "))
+
+media = (a+b)/2
+
+print(f"A média dos valores {a} e {b} é {media}")
--- Exercício 05.py
+++
@@ -1,6 +0,0 @@
-anoAtual = 2025
-anoNascimento = int(input("Digite o ano de nascimento: "))
-
-idade = anoAtual - anoNascimento
-
-print(f"A pessoa tem {idade} anos de idade")
