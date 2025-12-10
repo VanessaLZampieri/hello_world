67359c48e9756837c1f0602bf2cc76473a47a151
Vanessa Zampieri <vanessazampieri01@gmail.com>
2025-12-10 00:51:08 +0000

    Exercício 04 - Numeros Inteiros

---
+++ exercicio04.py
@@ -0,0 +1,9 @@
+a = int(input("Digite o primeiro numero: "))
+b = int(input("Digite o segundo numero: "))
+
+multiplica = a*b
+soma = a+b
+diminui = a-b
+divide = a//b
+
+print(f"resultado:\n 2x5 = {multiplica}\n 2+5 = {soma}\n 2-5 = {diminui}\n 2/5 = {divide}")
